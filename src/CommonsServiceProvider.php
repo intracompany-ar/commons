7b5f287a9117bec90b44bf308b0f1b35623ed842
Dux Ducis Arsen <alejandropalarich@live.com.ar>
2022-11-22 13:13:03 -0300

    Agrego scopes names

---
+++ src/CommonsServiceProvider.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace DuxDucisArsen\Commons;
+
+use Illuminate\Support\ServiceProvider;
+
+class CommonsServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        // $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+        // $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
+        // $this->loadViewsFrom(__DIR__.'/../resources/views', 'geography');
+    }
+}
