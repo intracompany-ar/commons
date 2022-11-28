d274f32cc89732dfd08f688b20cfe3088b24ea94
Dux Ducis Arsen <alejandropalarich@live.com.ar>
2022-11-27 23:25:57 -0300

    Agrego upload file csv

---
+++ src/Http/Requests/UploadFileCsvRequest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace DuxDucisArsen\Commons\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UploadFileCsvRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'file' => 'required|file|mimes:csv,xlsx,xls,txt'
+        ];
+    }
+}
