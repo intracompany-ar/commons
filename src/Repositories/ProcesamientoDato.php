687fa00a610c430aa368399f1fd6118b2e732f71
Dux Ducis Arsen <alejandropalarich@live.com.ar>
2022-12-01 08:24:25 -0300

    procesamiento datos

---
+++ src/Repositories/ProcesamientoDato.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace DuxDucisArsen\Commons\Repositories;
+
+class ProcesamientoDato
+{
+
+    /**
+     * Quita espacios de la cadena
+     *
+     */
+    static public function quitarEspacios($string)
+    {
+        return preg_replace('/\s+/', '', $string);
+    }
+
+    /**
+     * Reemplaza los espacio de la string
+     *
+     */
+    static public function reemplazarEspaciosPorSignoMas($string)
+    {
+        return preg_replace('/\s+/', '+', $string);
+    }
+
+    static public function extraerUltimos4CharNombreFile($fileSubido)
+    {
+        $nombre_file_subido = $fileSubido->getClientOriginalName();
+        return substr( $nombre_file_subido, 0, -4);
+    }
+
+
+
+
+}
