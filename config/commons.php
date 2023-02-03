e162f931282fb14b92b04c1ff9dc24305ac51995
Dux Ducis Arsen <alejandropalarich@live.com.ar>
2023-02-03 01:47:27 -0300

    Agrego config

---
+++ config/commons.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'theme-color' => '#d72f23',
+    'description' => 'IceO, estionar tu empresa va a ser un juego de niños. ERP a tu medida.',
+    'key-words'   => 'neumáticos, rueda, goma, automóvil, auto, camioneta, transporte, camión, acoplado, chasis, colectivo, agrícola, cosechadora, tractor, seguridad, alineación, balanceo, reconstrucción, vial, ruta, tracción , maquinaria, banda de rodamiento, motoniveladoras , palas cargadoras, suv, infladotires, providers, refate'
+];
+
+// Neuper, distribuidor oficial de neumáticos Fate en la zona sur de Provincia de Santa Fe, sur de Córdoba, norte de Buenos Aires y La Pampa. En sus sucursales se brinda servicios de gomeria, alineación, balanceo, tren delantero, frenos.
