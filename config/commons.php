dcb11ff29fe4f327d22c28ac7cb0286e3406cb97
Dux Ducis Arsen <alejandropalarich@live.com.ar>
2023-02-03 02:53:55 -0300

    Avances

--- config/commons.php
+++ config/commons.php
@@ -1,9 +1,26 @@
 <?php
 
 return [
+
+    /**
+     * Cuando se mira en movil es el color que toma Chrome para el encabezado
+     */
     'theme-color' => '#d72f23',
+
+    /**
+     * Meta tag. SEO
+     */
     'description' => 'IceO, estionar tu empresa va a ser un juego de niños. ERP a tu medida.',
-    'key-words'   => 'neumáticos, rueda, goma, automóvil, auto, camioneta, transporte, camión, acoplado, chasis, colectivo, agrícola, cosechadora, tractor, seguridad, alineación, balanceo, reconstrucción, vial, ruta, tracción , maquinaria, banda de rodamiento, motoniveladoras , palas cargadoras, suv, infladotires, providers, refate'
+    // Neuper, distribuidor oficial de neumáticos Fate en la zona sur de Provincia de Santa Fe, sur de Córdoba, norte de Buenos Aires y La Pampa. En sus sucursales se brinda servicios de gomeria, alineación, balanceo, tren delantero, frenos.
+
+    /**
+     * Meta tag. SEO
+     */
+    'key-words'   => 'neumáticos, rueda, goma, automóvil, auto, camioneta, transporte, camión, acoplado, chasis, colectivo, agrícola, cosechadora, tractor, seguridad, alineación, balanceo, reconstrucción, vial, ruta, tracción , maquinaria, banda de rodamiento, motoniveladoras , palas cargadoras, suv, infladotires, providers, refate',
+
+    /**
+     * Aparece continuamente luego del title
+     */
+    'subtitle-fix' => 'IceO Software'
 ];
 
-// Neuper, distribuidor oficial de neumáticos Fate en la zona sur de Provincia de Santa Fe, sur de Córdoba, norte de Buenos Aires y La Pampa. En sus sucursales se brinda servicios de gomeria, alineación, balanceo, tren delantero, frenos.
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
                 {{ auth()->user()->name }} ·
             @endauth
 
-            @yield('title') ·
+            @yield('title') · {{ config('commons.subtitle-fix') }}
 
             @intra
                 {{ config('app.title.intra') }}
@@ -36,7 +36,18 @@
         <meta name="keywords" content="{{ config('commons.key-words') }}">
         
         @intra
-            @include('commons::layouts._tags_metas_private')
+            <meta name='robots' content='noindex,nofollow'>
+            <meta name="googlebot" content="noindex">
+            <meta name="url-base" content="{{ config('app.url_intra') }}">
+            
+            @auth
+                <meta name="auth-id" content="{{ auth()->id() }}">
+                <meta name="auth-name" content="{{ auth()->user()->name }}">
+                <meta name="auth-email" content="{{ auth()->user()->email }}">
+                <meta name="person-id" content="{{ auth()->user()->person_id }}">
+                <meta name="person-first-name" content="{{ auth()->user()->person->first_name }}">
+                <meta name="profile-photo" content="{{ auth()->user()->profile_photo_url ? auth()->user()->profile_photo_url : asset('storage/img/bib.png') }}">
+            @endauth
         @else
             @include('commons::layouts._tags_metas_public')
         @endintra
