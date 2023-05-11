25c0b0e4755fae834d9d487bf666baae7ab063b6
Alejandro Palarich <duxducisarsen@gmail.com>
2023-05-11 02:29:23 -0300

    Cambio link de geogtq

--- resources/views/layouts/_head_standard.blade.php
+++ resources/views/layouts/_head_standard.blade.php
@@ -6,7 +6,7 @@
         font-family: 'Geogtq-Md';
         font-style: normal;
         font-weight: 400;
-        src: local('Geogtq-Md'), local('Geogtq-Md'), url( {{ asset('storage/fonts/Geogtq-Md.otf') }} ) format('opentype');
+        src: local('Geogtq-Md'), local('Geogtq-Md'), url( {{ asset('vendor/fonts/Geogtq-Md.otf') }} ) format('opentype');
         unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;/*@*/
     }
 </style>
