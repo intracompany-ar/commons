d8fe77ada1ab11127ac319571bd895f3df0c5dbb
Alejandro Palarich <duxducisarsen@gmail.com>
2023-05-16 01:24:39 -0300

    Agrego 4 vue components

---
+++ resources/js/components/forms/inputs/InputFechaDesde.vue
@@ -0,0 +1,31 @@
+
+<script setup>
+    import {ref} from 'vue';
+    import dayjs from 'dayjs';
+
+    const valorFinal = ref('2100-01-01');
+    const emit = defineEmits(['input']);
+    
+    const props = defineProps({
+        dias: { required: false, type: [String, Number], default: 30 },
+        big: { required: false, type: Boolean, default: false },
+        name: { required: false, type: String, default: 'desde' },
+        id: { required: false, type: String, default: 'desde' },
+        value: { required: false, type: String }
+    })
+
+    valorFinal.value = dayjs().subtract(props.dias, 'days').format("YYYY-MM-DD");
+</script>
+
+<template>
+    <label class="form-label" v-bind:for="props.id">Desde:</label>
+    <input type="date" class="form-control" 
+            v-bind:class="{'form-control-sm': !props.big}" 
+            v-bind:name="props.name" 
+            v-bind:id="props.id" 
+            v-bind:value="valorFinal"
+            v-on:input="emit('input', $event.target.value)"
+            >
+    <!-- <InputFechaDesde dias="30"></InputFechaDesde> -->
+</template>
+
