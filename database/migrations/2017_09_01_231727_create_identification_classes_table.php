d4279495371a91f71ddcf7e3d937d5a3db307203
Alejandro Palarich <duxducisarsen@gmail.com>
2023-12-12 08:41:48 -0300

    Update sap_id field to be nullable in identification_classes table

--- database/migrations/2017_09_01_231727_create_identification_classes_table.php
+++ database/migrations/2017_09_01_231727_create_identification_classes_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('identification_classes', function (Blueprint $table) {
             $table->increments('id');
             $table->smallinteger('afip_id')->unique()->comment('oficial AFIP');
-            $table->string('sap_id', 8)->unique()->comment('SAP FAte');
+            $table->string('sap_id', 8)->nullable()->unique()->comment('SAP FAte');
             $table->string('name', 50)->comment('oficial AFIP');
 
             $table->string('descripcion', 50)->nullable();
