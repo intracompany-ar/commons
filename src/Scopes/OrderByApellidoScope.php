de87e4f792fec3b01e018d38701110cdd3dc9e2e
Alejandro Palarich <duxducisarsen@gmail.com>
2024-02-04 14:45:07 -0300

    Cambio Apellido por apellido

--- src/Scopes/OrderByApellidoScope.php
+++ src/Scopes/OrderByApellidoScope.php
@@ -17,6 +17,6 @@ class OrderByApellidoScope implements Scope
      */
     public function apply(Builder $builder, Model $model)
     {
-        $builder->orderBy('Apellido');//->orderBy('id');// no usar id porque puede ser ambigua y aveces tira error cuando se hacen query sobre relaciones (porque id en gral lo tiene todas las tablas)
+        $builder->orderBy('apellido');//->orderBy('id');// no usar id porque puede ser ambigua y aveces tira error cuando se hacen query sobre relaciones (porque id en gral lo tiene todas las tablas)
     }
 }
