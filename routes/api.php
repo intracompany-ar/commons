4f449fac3f9b41dbd2c8dcb3aada6a2bc5bb9c4a
Alejandro Palarich <duxducisarsen@gmail.com>
2024-07-23 11:16:25 -0300

    Agrego middlewares .28

--- routes/api.php
+++ routes/api.php
@@ -6,18 +6,22 @@ use IntraCompany\Commons\Http\Controllers\VoucherClassController;
 use IntraCompany\Commons\Http\Controllers\IdentificationClassController;
 use Illuminate\Support\Facades\Route;
 
+Route::prefix('api')
+    ->middleware(['api', 'auth:sanctum'])
+    ->group(function ()
+    {
+        Route::get('voucherClass/select', [VoucherClassController::class, 'select'])->name('voucherClass.select');
 
-Route::get('voucherClass/select', [VoucherClassController::class, 'select'])->name('voucherClass.select');
+        Route::prefix('currency')
+            ->controller(CurrencyController::class)
+            ->group(function () {
+                Route::get('indexCommons', 'indexCommons');
+            });
 
-Route::prefix('currency')
-    ->controller(CurrencyController::class)
-    ->group(function () {
-        Route::get('indexCommons', 'indexCommons')->name('indexCommons');
+        Route::apiResources([
+            'voucherClass' => VoucherClassController::class,
+            'currency'              => CurrencyController::class,
+            'concepto' => ConceptoController::class,
+            'identificationClass'         => IdentificationClassController::class
+        ]);
     });
-
-Route::apiResources([
-    'voucherClass' => VoucherClassController::class,
-    'currency'              => CurrencyController::class,
-    'concepto' => ConceptoController::class,
-    'identificationClass'         => IdentificationClassController::class
- ]);
