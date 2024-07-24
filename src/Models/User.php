10697ddc528b289ba1b5cc0aa4a6a88626e8363a
Alejandro Palarich <duxducisarsen@gmail.com>
2024-07-24 15:50:23 -0300

    Agrego laravel sanctum para que user tenga api token

--- src/Models/User.php
+++ src/Models/User.php
@@ -9,10 +9,11 @@ use IntraCompany\Commons\Traits\UserRoles;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use UserRoles, HasFactory, Notifiable, SoftDeletes;
+    use HasApiTokens, UserRoles, HasFactory, Notifiable, SoftDeletes;
     
     /**
      * Get the attributes that should be cast.
