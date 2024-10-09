4466cffd8db8e54b82418d89a3383c0dbf10dc87
Alejandro Palarich <duxducisarsen@gmail.com>
2024-10-09 00:35:53 -0300

    Agreggo accessor nro_doc_guiones

--- src/Models/Company.php
+++ src/Models/Company.php
@@ -2,6 +2,7 @@
 
 namespace IntraCompany\Commons\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use IntraCompany\Commons\Traits\IdentificationClassable;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -14,6 +15,23 @@ class Company extends Model
     public $timestamps = false;
 
     protected $guarded = ['id'];
+
+    public $appends = ['nro_doc_guiones'];
+
+    public function nroDocGuiones(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($value, $attributes) => isset($attributes['entity_id']) ? (strlen($attributes['entity_id']) == 11
+                ?
+                substr($attributes['entity_id'], 0, 2).'-'.
+                substr($attributes['entity_id'], 2, 8).'-'.
+                substr($attributes['entity_id'], 10, 1)
+                :
+                $attributes['entity_id']
+            ) : null
+        );
+    }
+
 
     public function condicionIva(): BelongsTo
     {
