c95f0749d98192cf6ee28b3ffe7e9e45c182317b
Alejandro Palarich <duxducisarsen@gmail.com>
2024-10-11 19:28:19 -0300

    bug ruteo de fonts

--- resources/views/layouts/_head_standard.blade.php
+++ resources/views/layouts/_head_standard.blade.php
@@ -6,14 +6,14 @@
         font-family: 'Geogtq-Md';
         font-style: normal;
         font-weight: 400;
-        src: local('Geogtq-Md'), local('Geogtq-Md'), url( {{ asset('vendor/fonts/Geogtq-Md.otf') }} ) format('opentype');
+        src: local('Geogtq-Md'), local('Geogtq-Md'), url('fonts/Geogtq-Md.otf') format('opentype');
         unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;/*@*/
     }
 </style>
 
 <link rel="icon" type="image/x-icon" href="{{ asset(config('commons.logo.icon_32')) }}">
-<link rel="apple-touch-icon" sizes="180x180" href="{{ asset('storage/img/img_icons/icono_grupo_180.png') }}">
+<link rel="apple-touch-icon" sizes="180x180" href="{{ asset('img/img_icons/icono_grupo_180.png') }}">
 
 {{-- JAVASCRIPT --}}
-<script defer src="{{ asset('vendor/fonts/fonts.js') }}"></script>
+<script defer src="fonts/fonts.js"></script>
 @vite(config('commons.vite_files_js'))
