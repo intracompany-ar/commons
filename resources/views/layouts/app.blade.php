4ef530d32180948764eb1d08b9cf9f1c8e5cdeef
Alejandro Palarich <duxducisarsen@gmail.com>
2024-10-11 12:49:22 -0300

    Vuelvo a usar comopnents en head y tags porque necesito para guest

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,17 +8,7 @@
         {{-- Antes: config('app.url') --> No funcionan los links a id dentro de la misma página --> Lo cambio por url()->current() --}}
         <base href="{{ url()->current() }}">
 
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta http-equiv="Content-Type" content="text/html"; charset="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=5">
-        {{-- , user-scalable=no no hace falta mientras programe responsive --}}
-        <meta name="format-detection" content="telephone=no">
-        <meta name="author" content="IntraCompany">
-
-        @if (config('commons.pwa'))
-            <link rel="manifest" href="/manifest.json">
-        @endif
+        @include('commons::layouts._tags_metas_public')
         
         <meta name="app-id" content="{{ $applicationId ?? 1 }}">
         <meta name="csrf-token" content="{{ csrf_token() ?? 0 }}">
@@ -52,26 +42,7 @@
             <meta name="profile-photo" content="{{ auth()->user()->profile_photo_url ? auth()->user()->profile_photo_url : asset('storage/img/bib.png') }}">
         @endauth
 
-        @vite(config('commons.vite_files_style'))
-
-        <style media="screen">
-            /* Para que funcione Geogtq, es la que usa fate.com.ar. En lugar de secure_url deberia usar asset, pero en producción no se por qué toma http */
-            @font-face {
-                font-family: 'Geogtq-Md';
-                font-style: normal;
-                font-weight: 400;
-                src: local('Geogtq-Md'), local('Geogtq-Md'), url( {{ asset('vendor/fonts/Geogtq-Md.otf') }} ) format('opentype');
-                unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;/*@*/
-            }
-        </style>
-
-        <link rel="icon" type="image/x-icon" href="{{ asset(config('commons.logo.icon_32')) }}">
-        <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('storage/img/img_icons/icono_grupo_180.png') }}">
-
-        {{-- JAVASCRIPT --}}
-        <script defer src="{{ asset('vendor/fonts/fonts.js') }}"></script>
-        @vite(config('commons.vite_files_js'))
-
+        @include('commons::layouts._head_standard')
         @stack('scriptsIni')
     </head>
 
