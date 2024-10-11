169f5743f182e7cc9a07dd9599e4b3cb80b961b5
Alejandro Palarich <duxducisarsen@gmail.com>
2024-10-11 19:44:58 -0300

    carga de fuentes condicional2

--- resources/views/layouts/_head_standard.blade.php
+++ resources/views/layouts/_head_standard.blade.php
@@ -12,10 +12,9 @@
             unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;/*@*/
         }
     </style>
-    {{-- JAVASCRIPT --}}
-    <script defer src="fonts/fonts.js"></script>
 @endif
 
+<script defer src="fonts/fonts.js"></script>
 <link rel="icon" type="image/x-icon" href="{{ asset(config('commons.logo.icon_32')) }}">
 <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('img/img_icons/icono_grupo_180.png') }}">
 
